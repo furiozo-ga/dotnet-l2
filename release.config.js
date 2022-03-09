88526a43b7aca709a9cf45c438a45c620acd0915
furiozo <nikolay.panayotov@eldersoss.com>
2022-03-09 15:48:13 +0200

    fix: t1

--- release.config.js
+++ release.config.js
@@ -18,7 +18,7 @@ module.exports = {
     prepare: [
         {
             path: '@semantic-release/exec',
-            cmd: `echo aaa`,
+            cmd: `echo aaa ------`,
         },
 
 //        {
